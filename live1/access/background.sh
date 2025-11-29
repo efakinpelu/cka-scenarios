fa1f0ea581ff828a2ec755d19cfec0b5d1d8d77d
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 16:51:08 +0100

    Update background.sh

--- live1/access/background.sh
+++ live1/access/background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -a validUser;
 cat <<EOF >/tmp/users 
-  6a7acd8c717ac027458a4a8a6dfdf12c 
-  6a7acd8c717ac027458a4a8a6dfdf134
+6a7acd8c717ac027458a4a8a6dfdf12c 
+6a7acd8c717ac027458a4a8a6dfdf134
 EOF
