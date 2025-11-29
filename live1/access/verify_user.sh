a131d4466457b5aac10160a4d96162f4a72e0859
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 17:56:30 +0100

    Update verify_user.sh

--- live1/access/verify_user.sh
+++ live1/access/verify_user.sh
@@ -2,6 +2,12 @@
 
 mapfile -t users < /tmp/users
 for user in ${users[@]}; do
-  if [[ $user == $envUser ]]; then valid=true fi
+  if [[ $user == $envUser ]]; then 
+    valid=true;
+    echo "user found"
+  fi
 done
-if !$valid; then exit 1; fi
+if !$valid; then
+  echo "user not found"
+  exit 1; 
+fi
