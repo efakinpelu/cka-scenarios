672c154cfbaddfbf81087fbd6f411ae1feec8e37
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 13:55:01 +0100

    Create verify.sh

---
+++ live1/access/verify.sh
@@ -0,0 +1,10 @@
+declare -a validUser;
+validUser = (6a7acd8c717ac027458a4a8a6dfdf12c 6a7acd8c717ac027458a4a8a6dfdf134);
+for user in ${validUser[@]}; do
+  if [[ $user == $envUser ]]; then
+      valid=true
+  fi
+done
+if $valid; then 
+  exit(0)
+fi
