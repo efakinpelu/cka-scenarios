a3d3107d0ccfa1ea35a8c611b1f180350aee0106
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 14:56:14 +0100

    Update verify.sh

--- live1/access/verify.sh
+++ live1/access/verify.sh
@@ -1,3 +1,4 @@
+!/bin/bash
 declare -a validUser;
 validUser=(6a7acd8c717ac027458a4a8a6dfdf12c 6a7acd8c717ac027458a4a8a6dfdf134);
 for user in ${validUser[@]}; do
@@ -6,5 +7,5 @@ for user in ${validUser[@]}; do
   fi
 done
 if [[ -n $valid ]]; then 
-  echo "OK"
+  export userVali="OK"
 fi
