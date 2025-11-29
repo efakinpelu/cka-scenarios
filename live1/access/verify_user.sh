d62831981554844a1e4c11ea186716e1ce32b6b3
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 18:10:20 +0100

    Update verify_user.sh

--- live1/access/verify_user.sh
+++ live1/access/verify_user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mapfile -t users < /tmp/users
-for user in ${users[@]}; do if [[ $user == $envUser ]]; then valid=true; fi done;
+for user in ${users[@]}; do if [[ $user==$envUser ]]; then valid=true; fi; done;
 if !$valid; then exit 1; fi;
