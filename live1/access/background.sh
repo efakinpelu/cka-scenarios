ee1e4e2683948b7fe153661b50612ed61f5a17f6
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 16:26:51 +0100

    Update background.sh

--- live1/access/background.sh
+++ live1/access/background.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 declare -a validUser;
-export validUser=(6a7acd8c717ac027458a4a8a6dfdf12c 6a7acd8c717ac027458a4a8a6dfdf134);
+echo export validUser=(6a7acd8c717ac027458a4a8a6dfdf12c 6a7acd8c717ac027458a4a8a6dfdf134);
