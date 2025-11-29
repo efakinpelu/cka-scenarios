8ac8165b72eaffc005651cb17ba9bf99137e6f6c
efakinpelu <142757942+efakinpelu@users.noreply.github.com>
2025-11-29 16:08:50 +0100

    Update verify_user.sh

--- live1/access/verify_user.sh
+++ live1/access/verify_user.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-declare -a validUser;
-validUser=(6a7acd8c717ac027458a4a8a6dfdf12c 6a7acd8c717ac027458a4a8a6dfdf134);
 for user in ${validUser[@]}; do
   if [[ $user == $envUser ]]; then
       valid=true
   fi
 done
-if [[ -n $valid ]]; then 
-  export userValid="OK"
-fi
+if $valid; then exit 0; fi
